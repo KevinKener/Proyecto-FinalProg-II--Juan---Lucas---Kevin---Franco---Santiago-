19473f3243d0acb20dd4712edac8717503d33e2d
Lucas Luppi <lucasgluppi@gmail.com>
2024-11-13 01:27:19 -0300

    Update validacion.js
    arreglo validaciones en tiempo real del formulario de contacto

--- app/static/js/validacion.js
+++ app/static/js/validacion.js
@@ -1,106 +1,109 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const form = document.getElementById("contactForm");
-    form.addEventListener("submit", function (evento){
-            let valido = true;
+const nombreIngresado = document.getElementById("name");
+const emailIngresado = document.getElementById("email");
+const msjIngresado = document.getElementById("message");
+const asuntoIngresado = document.getElementById("subject");
 
-            let name = document.getElementById("name").value.trim(); /* me de contenido del campo */
-            let email = document.getElementById("email").value.trim();
-            let subject = document.getElementById("subject").value.trim();
-            let message = document.getElementById("message").value.trim();
-            
-            let primerCampoConError = false;
+const nombreError = document.getElementById("errorName");
+const emailError = document.getElementById("errorEmail");
+const msjError = document.getElementById("errorMessage");
+const asuntoError = document.getElementById("errorSubject");
 
-            // Expresiones regulares para validación
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        
-            //Validar Nombre
-        
-            if (name.split(' ').length < 2) {
-                valido = false
-                mostrarError("errorName", "El nombre debe tener al menos 2 palabras")
-                if(!primerCampoConError) primerCampoConError = document.getElementById("name");
-            }   
-            //Validacion de email
-            
-            if (!emailRegex.test(email)){
-                valido = false
-                mostrarError("errorEmail" , "El correo electronico no es valido");
-                if(!primerCampoConError) primerCampoConError = document.getElementById("email");
-            } else if (email.length < 5 || email.length > 254 ){
-                valido = false
-                mostrarError("errorEmail" , "La longitud del correo electrónico debe estar entre 5 y 254 caracteres.");
-                if(!primerCampoConError) primerCampoConError = document.getElementById("email");
-            }
-            //Validacion de Asunto
-            if (subject.length < 5) {
-                valido = false
-                mostrarError("errorSubject", "El asunto debe tener al menos 5 caracteres")
-                if(!primerCampoConError) primerCampoConError = document.getElementById("subject");
-            }  
-            if (message.length < 15) {
-                valido = false
-                mostrarError("errorMessage", "El mensaje es demasiado corto ingrese un mensaje con al menos 15 caracteres")
-                if(!primerCampoConError) primerCampoConError = document.getElementById("message");
-            }  
-            if(!valido){
-                evento.preventDefault();
-                if(primerCampoConError) primerCampoConError.focus()
-            }
-            else{
-                alert("Envio Exitoso!!");
-                location.reload();
-            }
-    });
-    //Eventos input
-    document.getElementById("name").addEventListener("input", () => validarNombre());
-    document.getElementById("email").addEventListener("input", () => validarEmail());
-    document.getElementById("subject").addEventListener("input", () => validarAsunto());
-    document.getElementById("message").addEventListener("input", () => validarMensaje());
+// Declaro funciones de validación
 
-    //Funcion validar nombre
-    function validarNombre(name) {
-        if (name.split(' ').length >= 2) {
-            ocultarError("errorName");
-        } else {
-            mostrarError("errorName", "El nombre debe tener al menos 2 palabras");
-        }
-    }
-    //Funcion validar Email
-    function validarEmail(email) {
-        if (emailRegex.test(email) && email.length >= 5 && email.length <= 254) {
-            ocultarError("errorEmail");
-        } else if (!emailRegex.test(email)) {
-            mostrarError("errorEmail", "El correo electrónico no es válido");
-        } else {
-            mostrarError("errorEmail", "La longitud del correo electrónico debe estar entre 5 y 254 caracteres.");
-        }
-    }
-    //funcion validar asunto
-    function validarAsunto(subject) {
-        if (subject.length >= 5){
-            ocultarError("errorSubject")
-        } else{
-            mostrarError("errorSubject", "El asunto debe tener al menos 5 caracteres")            
-        }
-    }
-    //funcion validar mensaje
-    function validarMensaje(message) {
-        if(message.length > 15){
-            ocultarError("errorMessage")
-        }else{
-            mostrarError("errorMessage", "El mensaje es demasiado corto ingrese un mensaje con al menos 15 caracteres")
-        }
-    }
-    // Función para mostrar errores
-    function mostrarError(id, mensaje) {
-        const errorElement = document.getElementById(id);
-        errorElement.textContent = mensaje;
-        errorElement.classList.add("error-text");
-    }
+function validarNombre() {
+  const ERLetras = /^[A-Za-z\s]+$/;
+  const nombre = nombreIngresado.value.trim();
 
-    // Función para ocultar los mensajes de error
-    function ocultarError(id) {
-        const errorElement = document.getElementById(id);
-        errorElement.textContent = ""; // Borra el contenido del mensaje de error
-    }    
+  //validaciones
+  if (nombre === "") {
+    nombreError.textContent = "Ingrese nombre";
+    return false;
+  }
+  if (nombre.split(' ').length < 2)
+  {
+    nombreError.textContent = "El nombre debe tener al menos 2 palabras";
+    return false;
+  }
+  if (!ERLetras.test(nombre)) {
+    nombreError.textContent = "El nombre no debe contener caracteres especiales";
+    return false;
+  }
+  if (nombre.length < 3) {
+    nombreError.textContent = "El nombre debe tener al menos 3 caracteres";
+    return false;
+  }
+
+  nombreError.textContent = ""; //si todo esta okey, limpia el contenido de la clase del error (es decir, que no aparezca más)
+  return true;
+}
+
+function validarEmail() {
+  const EREmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = emailIngresado.value.trim();
+
+  //Validaciones
+  if (email === "") {
+    emailError.textContent = "Ingrese email";
+    return false;
+  }
+  if (!EREmail.test(email)) {
+    emailError.textContent = "Email inválido. Verifique el formato";
+    return false;
+  }
+
+  emailError.textContent = "";
+  return true;
+}
+
+function validarAsunto() {
+  const asunto = asuntoIngresado.value.trim();
+
+  //Validaciones
+  if (asunto.length < 5) {
+    asuntoError.textContent = "El asunto debe tener al menos 5 caracteres";
+    return false;
+  }
+
+  asuntoError.textContent = "";
+  return true;
+}
+
+function validarMensaje() {
+  const msj = msjIngresado.value.trim();
+
+  //Validaciones
+  if (msj.length < 10) {
+    msjError.textContent = "El mensaje debe tener al menos 10 caracteres";
+    return false;
+  }
+
+  msjError.textContent = "";
+  return true;
+}
+
+// Agrego evento blur a cada input, esto hará que cuando se desenfoque muestre inmediatamente si hay algún error
+
+nombreIngresado.addEventListener("blur", validarNombre);
+emailIngresado.addEventListener("blur", validarEmail);
+asuntoIngresado.addEventListener("blur", validarAsunto);
+msjIngresado.addEventListener("blur", validarMensaje);
+
+
+// Por último, solo queda prevenir que el formulario se envíe teniendo errores con la línea 153
+// De hecho, no sólo se debe prevenir si no que al llamar a cada una de las funciones de validación
+// se marcan los errores de aquellos inputs que no hayan sido enfocadas antes (lo que no arrojaría mensaje de error)
+
+const formulario = document.getElementById("contactForm");
+
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const validez_nombre = validarNombre();
+  const validez_email = validarEmail();
+  const validez_asunto = validarAsunto();
+  const validez_msj = validarMensaje();
+
+  if (validez_nombre && validez_email && validez_asunto && validez_msj) {
+    formulario.submit();
+    alert("Formulario enviado");
+  }
 });
