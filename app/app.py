a8a96fd321022118b06407c45f70828675c9726f
Juan Pablo Fernandez <149335938+JuanPabloFernandezAlvarez@users.noreply.github.com>
2024-11-13 15:18:55 -0300

    app.py comentado

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
+#clases importadas
 from flask import Flask, render_template, redirect, url_for
 
+#Inicializa la app
 app=Flask(__name__)
 
+#metodo para indicar que esta es la ruta raiz
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -9,7 +12,7 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     
-    return redirect(url_for('index'))
+    return redirect(url_for('index')) #metodo vinculado para la redireccion a la pagina principal
 
 @app.route('/Login')
 def login():
@@ -22,7 +25,7 @@ def Carrito():
 def pagina_no_encontrada(error):
     return render_template('404.html'),404
     
-
+#condicion que comprueba si estamos en la app(la lanza)
 if __name__ == '__main__':
     app.register_error_handler(404, pagina_no_encontrada)
-    app.run(debug=True)
+    app.run(debug=True)#debug en true para hacer las modificaciones en tiempo real
